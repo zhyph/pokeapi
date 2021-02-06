fbc52cf1567edd1f25c8e075f1603341e15eef11
Mauro <exdefalt951@gmail.com>
2021-02-06 14:00:00 -0300

    codigo alterado

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,25 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const [poke, setPoke] = useState([]); //recebe pokemons
-  const [pokeID, setPokeID] = useState(""); //recebe pokemonid
-  const [submit, setSubmit] = useState("");
-  const [query, setQuery] = useState("bulbasaur");
-  const [id, setId] = useState(1);
+export default function App() {
 
-  useEffect(() => {
-    getPokemons();
-  }, [query]);
+  const [nome, setnome] = useState([]);
+  const [imagem, setimagem] = useState([]);
 
-  function getNumbers(inputString) {
-    var regex = /\d+\.\d+|\.\d+|\d+/g,
-      results = [],
-      n;
-
-    while ((n = regex.exec(inputString))) {
-      results.push(parseFloat(n[0]));
-    }
-
-    return results;
-  }
-
-  const getUrlId = (_) => {
-    poke.map((item) => {
-      if (item.name === query) {
-        const res = getNumbers(item.url);
-        setId({ id: res[1] });
-        // console.log(`${res[1]}`)
-      }
-    });
-  };
-
-  const getPokeId = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + id);
-    // console.log(response)
+  const getPokemons = async () => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`+nome);
     const data = await response.json();
-    setPokeID(data);
+    setimagem(data.sprites.other["official-artwork"].front_default)
+    console.log(imagem)  
     console.log(data);
   };
 
-  const getPokemons = async () => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
-    // console.log(response)
-    const data = await response.json();
-    setPoke(data.results);
-    console.log(data.results);
-  };
-
-  const updateSearch = (e) => {
-    setSubmit(e.target.value);
-  };
-
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(submit);
-    setSubmit("");
-    getUrlId();
-    getPokeId();
-  };
-
-  const fodase = () => {
-    if (pokeID !== "") {
-      return (
-        <img
-          src={pokeID.sprites.other.dream_world.front_default}
-          alt="pokeman"
-        />
-      );
-    }
-  };
-
   return (
-    <div className="App">
-      <h1>Ulgy pokemons</h1>
-      <form onSubmit={getSearch}>
-        <input
-          type="text"
-          placeholder="Choose a pokemon"
-          onChange={updateSearch}
-        />
-        <button id="btn" type="submit">
-          GO!
-        </button>
-      </form>
-      {pokeID.height}
-      {fodase()}
+    <div className="App" style={{display:'flex' , flexDirection:'column'}}>
+            <input type='text' placeholder='Nome do Pokemon' onChange={(e)=>{setnome(e.target.value)}}/>
+      <button onClick={getPokemons}>Buscar</button>
+      <img src={imagem}/>
     </div>
   );
 }
 
-export default App;
