3efd57bfb91919dd2e5fd89a2561d9adea773d1d
Mauro <exdefalt951@gmail.com>
2021-02-06 14:23:55 -0300

    codigo alterado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { getElementError } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import imgpadrao from './assets/buscar.png'
 
 export default function App() {
 
-  const [nome, setnome] = useState([]);
-  const [imagem, setimagem] = useState([]);
+  const [nome, setnome] = useState();
+  const [imagem, setimagem] = useState(false);
 
   const getPokemons = async () => {
+  document.getElementById('buttonbuscar').value = ''
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`+nome);
     const data = await response.json();
     setimagem(data.sprites.other["official-artwork"].front_default)
@@ -15,11 +18,13 @@ export default function App() {
   };
 
   return (
-    <div className="App" style={{display:'flex' , flexDirection:'column'}}>
-            <input type='text' placeholder='Nome do Pokemon' onChange={(e)=>{setnome(e.target.value)}}/>
-      <button onClick={getPokemons}>Buscar</button>
-      <img src={imagem}/>
+ 
+    <div className="App" style={{display:'flex' , flexDirection:'column' , alignItems:'center' , justifyContent:'center' , marginTop:"30px"}}>
+            <input id='buttonbuscar' style={{margin:'20px'}} type='text' placeholder='Nome do Pokemon' onChange={(e)=>{setnome(e.target.value)}}/>
+      <button  style={{margin:'20px'}}  onClick={getPokemons}>Buscar</button>
+      <img width='200px' height='200px' src={imagem ? imagem : imgpadrao}/>
     </div>
+
   );
 }
 
