3c9e90853dd82cfbd8ead80970a97d06291e0b54
Mauro <exdefalt951@gmail.com>
2021-02-06 14:33:42 -0300

    codigo alterado

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,44 @@ export default function App() {
   const [imagem, setimagem] = useState(false);
 
   const getPokemons = async () => {
-  document.getElementById('buttonbuscar').value = ''
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/`+nome);
-    const data = await response.json();
-    setimagem(data.sprites.other["official-artwork"].front_default)
-    console.log(imagem)  
-    console.log(data);
+
+    if (nome) {
+      document.getElementById('buttonbuscar').value = ''
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + nome);
+      const data = await response.json();
+      setimagem(data.sprites.other["official-artwork"].front_default)
+      console.log(imagem)
+      console.log(data);
+      setnome(false)
+    } else {
+      window.alert('Precisa inserir um nome')
+    }
   };
 
   return (
- 
-    <div className="App" style={{display:'flex' , flexDirection:'column' , alignItems:'center' , justifyContent:'center' , marginTop:"30px"}}>
-            <input id='buttonbuscar' style={{margin:'20px'}} type='text' placeholder='Nome do Pokemon' onChange={(e)=>{setnome(e.target.value)}}/>
-      <button  style={{margin:'20px'}}  onClick={getPokemons}>Buscar</button>
-      <img width='200px' height='200px' src={imagem ? imagem : imgpadrao}/>
+
+    <div className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: "30px"
+      }}>
+
+      <input id='buttonbuscar'
+        style={{ margin: '20px' }}
+        type='text'
+        placeholder='Nome do Pokemon'
+        onChange={(e) => { setnome(e.target.value) }} />
+
+      <button style={{ margin: '20px' }}
+        onClick={getPokemons}>Buscar</button>
+
+      <img width='200px'
+        height='200px'
+        src={imagem ? imagem : imgpadrao} />
+
     </div>
 
   );
